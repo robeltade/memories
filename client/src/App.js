092dc000d8903601a166ecfb961e2a336552ee2b
robel tadesse <robeltadesse37@gmail.com>
2024-04-09 05:20:03 +1200

    setting up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
+import React from 'react'
+import './App.css'
 
-
-
-function App() {
+const App = () => {
   return (
-    <div >
-      <h1>App</h1>
-    </div>
-  );
+    <h1>App</h1>
+  )
 }
 
-export default App;
+export default App
